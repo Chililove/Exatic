8e9ec09bf1db81fedd21950ebc9ab1489a5b318e
suon0001 <56873514+suon0001@users.noreply.github.com>
2022-10-10 13:45:10 +0200

    Signup.php

--- public/login.php
+++ public/login.php
@@ -10,7 +10,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="../css/css.scss">
+    <link rel="stylesheet" href="../dist/css.scss">
     <title>login</title>
 </head>
 <body>
