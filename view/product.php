854980d943ae6baae485fa4d54ab5c11aead8063
suon0001 <56873514+suon0001@users.noreply.github.com>
2022-10-12 19:40:05 +0200

    Design product-cards

--- view/product.php
+++ view/product.php
@@ -1,1 +1,279 @@
-<h1>This is Product Page</h1>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>product</title>
+</head>
+<body>
+
+<section style="background-color: #eee;">
+    <div class="text-center container py-5">
+        <h4 class="mt-4 mb-5"><strong>Bestsellers</strong></h4>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image hover-zoom ripple ripple-surface ripple-surface-light"
+                         data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5><span class="badge bg-primary ms-2">New</span></h5>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">$61.99</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image ripple ripple-surface ripple-surface-light"
+                         data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5><span class="badge bg-danger ms-2">-20%</span>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">$61.99</h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5><span class="badge bg-danger ms-2">-10%</span></h5>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">
+                            <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5>
+                                        <span class="badge bg-danger ms-2">-10%</span>
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">
+                            <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5>
+                                        <span class="badge bg-primary ms-2">New</span><span
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">
+                            <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5>
+                                        <span class="badge bg-primary ms-2">New</span><span
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">
+                            <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image hover-zoom ripple" data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5>
+                                        <span class="badge bg-primary ms-2">New</span><span
+                                    </h5>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">
+                            <s>$61.99</s><strong class="ms-2 text-danger">$50.99</strong>
+                        </h6>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 mb-4">
+                <div class="card" style="background: #c3dbb6">
+                    <div class="bg-image ripple ripple-surface ripple-surface-light"
+                         data-mdb-ripple-color="light">
+                        <img src="https://img.freepik.com/free-vector/banner-asian-food-restaurant-with-black-white-doodles_125540-1348.jpg"
+                             class="w-100" />
+                        <a href="#!">
+                            <div class="mask">
+                                <div class="d-flex justify-content-start align-items-end h-100" style="background: #ccccff;">
+                                    <h5><span class="badge bg-danger ms-2">-20%</span>
+                                </div>
+                            </div>
+                            <div class="hover-overlay">
+                                <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="card-body">
+                        <a href="" class="text-reset">
+                            <h5 class="card-title mb-3">Product name</h5>
+                        </a>
+                        <a href="" class="text-reset">
+                            <p>Category</p>
+                        </a>
+                        <h6 class="mb-3">$61.99</h6>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <nav class="d-flex justify-content-center">
+        <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+    </nav>
+</section>
+
+<style lang="scss">
+    body {
+        background: #4CAF50;
+    }
+
+
+</style>
+
+</body>
+</html>
