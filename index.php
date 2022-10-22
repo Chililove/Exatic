a2bc33fa890775e60edd2fc6c17f86d3ec82af11
suon0001 <56873514+suon0001@users.noreply.github.com>
2022-10-22 14:20:10 +0200

    Revert "Merge pull request #5 from Chililove/frontpage-design"
    This reverts commit a2b5b6034062e36215e15c8fa93e204dd1b8ef6c, reversing
    changes made to 8e9ec09bf1db81fedd21950ebc9ab1489a5b318e.

--- index.php
+++ index.php
@@ -28,6 +28,8 @@
 
 $request = $_SERVER['REQUEST_URI'];
 
+echo $request;
+
 switch ($request) {
     case '':
     case '/':
@@ -35,37 +37,37 @@ switch ($request) {
         break;
 
     case '/about-us':
-        require __DIR__ . '/view/aboutus.php';
+        require __DIR__ . '/public/aboutus.php';
         break;
 
     case '/contact':
-        require __DIR__ . '/view/contact.php';
+        require __DIR__ . '/public/contact.php';
         break;
     case '/product-overview':
-        require __DIR__ . '/view/productoverview.php';
+        require __DIR__ . '/public/productoverview.php';
         break;
     case '/frontpage':
-        require __DIR__ . '/view/frontpage.php';
+        require __DIR__ . '/public/frontpage.php';
         break;
     case '/signin':
-        require __DIR__ . '/view/login.php';
+        require __DIR__ . '/public/login.php';
         break;
     case '/signup':
-        require __DIR__ . '/view/signup.php';
+        require __DIR__ . '/public/signup.php';
         break;
     case '/profile':
-        require __DIR__ . '/view/profile.php';
+        require __DIR__ . '/public/profile.php';
         break;
     case '/checkout':
-        require __DIR__ . '/view/checkout.php';
+        require __DIR__ . '/public/checkout.php';
         break;
 
     case '/product':
-        require __DIR__ . '/view/product.php';
+        require __DIR__ . '/public/product.php';
         break;
 
     default:
         http_response_code(404);
-        require __DIR__ . '/view/404.php';
+        require __DIR__ . '/public/404.php';
         break;
 }
---
+++ public/profile.php
@@ -0,0 +1,1 @@
+<h1>This is Profile</h1>
