d24cf05b6e3ec09eb445a4564203391868ad6903
Louise Chili <chili@Louises-MacBook-Pro.local>
2022-11-08 16:35:10 +0100

    display pic from db, delete unused code and moved textbox in frontpage.php

--- View/frontpage.php
+++ View/frontpage.php
@@ -10,7 +10,7 @@ require_once("connection/conn.php");
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>
 
-    <div style="width: 200px; font-size: 20px; background-image: linear-gradient(to left, darkgreen, lightgreen, lightskyblue, lightgreen, lightpink, lightgreen);
+    <div style="width: 200px; font-size: 30px; background-image: linear-gradient(to left, darkgreen, lightgreen, lightskyblue, lightgreen, lightpink, lightgreen);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   margin-right:auto;
@@ -20,22 +20,31 @@ require_once("connection/conn.php");
         <br>
         <h1>Welcome to Exatic!</h1>
     </div>
+    <br><br>
+    <div>
+        <div class="boxed" id="text-box">
+            It's very nice to have you here, we hope the experience will please you.<br />Here at Exatic we aim to broaden asian products as well as making them more accessible in Denmark.<br /><br />
+            Here you can find any ingredient you need to cook asian cuisine and treat family and friends with familiar and newly added products.<br />
+            All groceries can be deliveried at home.<br />
+            <br />
+            Hopefully you find what you need or maybe get inspired by new products from your familiar brands.
+        </div>
+        <div class="discountedItem">
+            <!--VOUCHER-->
+            <div class="card voucherstop">
 
-    <div class="discountedItem">
-        <!--VOUCHER-->
-        <div class="card voucherstop">
+                <div class="embed-responsive embed-responsive-16by9">
 
-            <div class="embed-responsive embed-responsive-16by9">
+                    <div class="voucher-divider">
+                        <div class="voucher-left text-center">
+                            <span class="voucher-nametop" style="font-size: 110%;">A special daily offer</span>
+                            <h5 class="voucher-codetop" style="font-size: 100%;">#discount</h5>
 
-                <div class="voucher-divider">
-                    <div class="voucher-left text-center">
-                        <span class="voucher-nametop" style="font-size: 110%;">A special daily offer</span>
-                        <h5 class="voucher-codetop" style="font-size: 100%;">#discount</h5>
-
-                    </div>
-                    <div class="voucher-righttop text-center border-left">
-                        <h5 class="discount-percent">20%</h5>
-                        <span class="off">Off</span>
+                        </div>
+                        <div class="voucher-righttop text-center border-left">
+                            <h5 class="discount-percent">20%</h5>
+                            <span class="off">Off</span>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -56,18 +65,19 @@ require_once("connection/conn.php");
             <div class="card" id="cardtop">
                 <div class="embed-responsive embed-responsive-16by9">
 
-                    <a href="/productoverview"><img class="card-img-top-top" src="data:image/jpeg;base64,<?php echo base64_encode($row['productImage']) ?>" alt="Card image top" /></a>
+                    <a href="/Exatic/product-overview?"><img class="card-img-top-top" src="/Exatic/assets/<?php echo $row['productImage'] ?>" alt="Card image top" /></a>
                     <div class="card-body">
                         <h3 class="card-title"><?php echo $row["title"] ?>
                             <?php if ($row['isDailySpecial']) {
                                 echo '<span class="badge bg-warning">Daily Special</span>';
                             } ?>
                         </h3>
-                        <h4 class="card-subtitle"><?php echo $row["description"] ?></h4>
+                        <h4 class="card-subtitle"><?php echo substr($row["description"], 0, 45); ?></h4>
                         <p class="card-text-price text-end">Price<?php echo $row["price"] ?>dkk</p>
+                        <p class="card-text"> <?php echo $row["stockQuantity"] ?> items left</p>
                     </div>
                     <div class="card-footer text-end">
-                        <p class="card-text"> <?php echo $row["stockQuantity"] ?> items left</p>
+
                         <a href="/shoppingcart"> <button type="button" class="btn btn-primary" onclick="">Add to cart</button></a>
                     </div>
                 </div>
@@ -79,127 +89,7 @@ require_once("connection/conn.php");
     }
 
     ?>
-    <span><br><br><br></span>
 
-    <div class="boxed" id="text-box" style="margin-top: -24%;">
-        It's very nice to have you here, we hope the experience will please you.<br />Here at Exatic we aim to broaden asian products as well as making them more accessible in Denmark.<br /><br />
-        Here you can find any ingredient you need to cook asian cuisine and treat family and friends with familiar and newly added products.<br />
-        All groceries can be deliveried at home.<br />
-        <br />
-        Hopefully you find what you need or maybe get inspired by new products from your familiar brands.
-    </div>
-
-    <!--
-    <div carousel>
-        <div class="container px-0" id="containermain">
-            <div class="container" id="container" style="padding: 0;">
-                <div class="row justify-content-center">
-                    <div class="col-md-4 col-md-offset-2">
-
-                        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
-                            <div class="carousel-inner">
-                                <div class="carousel-item active">
-                                    <img src="Exatic/assets/noodles.jpeg" class="img-fluid w-100" alt="green">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="Exatic/assets/matcha.jpeg" class="img-fluid w-100" alt="pink">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="Exatic/assets/drink.jpeg" class="img-fluid w-100" alt="study">
-                                </div>
-                                <div class="carousel-item">
-                                    <img src="Exatic/assets/can.jpeg" class="img-fluid rounded thumbnail-image" alt="">
-                                </div>
-                            </div>
-                            <a class="carousel-control-prev" href="#carousel" role="button" data-bs-slide="prev">
-                                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Previous</span>
-                            </a>
-                            <a class="carousel-control-next" href="#carousel" role="button" data-bs-slide="next">
-                                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                                <span class="visually-hidden">Next</span>
-                            </a>
-                        </div>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </div> -->
-    <!--
-<div class="container">
-    <div class="card-deck">
-        <div class="card">
-            <div class="embed-responsive embed-responsive-16by9">
-
-                <img class="card-img-top embed-responsive-item" src="Exatic/assets/can.jpeg" alt="Card image top">
-                <div class="card-body">
-                    <h3 class="card-title">Rita Kiwi</h3>
-                    <h4 class="card-subtitle">Vietnamese Kiwi Drink</h4>
-                    <p class="card-text">2 Items left</p>
-                    <p class="card-text-price">Price (20Dkr.)</p>
-                    <button class="buybtn" onclick="smt">Add to cart</button>
-                </div>
-            </div>
-        </div>
-    </div>
-</div> #carousel {
-            position: absolute;
-            top: 60%;
-            right: 0;
-            bottom: 0;
-            width: 28%;
-            opacity: .8;
-            filter: alpha(opacity=80);
-            font-size: 20px;
-            color: #fff;
-            text-align: center;
-            text-shadow: 0 1px 2px rgba(0, 0, 0, 0.6);
-            border-radius: 100px 1px 0px 1px;
-            /*TL TR BR BL*/
-            overflow: hidden;
-        }
-         .image-container {
-
-            position: relative;
-
-        }
-
-        .thumbnail-image {
-            border-radius: 10px !important;
-
-        }
-
-
-         .btn2 {
-            border-radius: 30%;
-            border-style: groove;
-            border-color: lightgreen;
-            padding: 3px;
-            background-color: lightcyan;
-        }
-
-        .item-size {
-            width: 15px;
-            height: 15px;
-            border-radius: 50%;
-            background: #fff;
-            border: 1px solid grey;
-            color: grey;
-            font-size: 10px;
-            text-align: center;
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-  .off {
-            font-size: 100%;
-            position: relative;
-            bottom: 5px;
-        }
-
-
--->
     <!-- Products array cards limit 3 new -->
 
 
@@ -208,10 +98,9 @@ require_once("connection/conn.php");
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
   margin-left: 2.9%;
-  margin-top: 2%;
+  margin-top: -6%;
   padding:2%;
     ">
-            <br><br><br>
             <h1>New products in store..</h1>
         </div>
     </span>
@@ -229,15 +118,17 @@ require_once("connection/conn.php");
                         <div class="card">
                             <div class="embed-responsive embed-responsive-16by9">
 
-                                <a href="/productoverview"><img class="card-img-top" src="data:image/jpeg;base64,<?php echo base64_encode($row['productImage']) ?>" alt="Card image top" /></a>
+                                <a href="/Exatic/product-overview?"><img class="card-img-top" src="/Exatic/assets/<?php echo $row['productImage'] ?>" alt="Card image top" /></a>
                                 <div class="card-body">
                                     <h3 class="card-title"><?php echo $row["title"] ?>
                                         <?php if ($row['isNew']) {
                                             echo '<span class="badge bg-success">New</span>';
                                         } ?>
                                     </h3>
-                                    <h4 class="card-subtitle"><?php echo $row["description"] ?></h4>
+                                    <h4 class="card-subtitle"><?php echo substr($row["description"], 0, 65); ?>...</h4>
+
                                     <p class="card-text"> <?php echo $row["stockQuantity"] ?> items left</p>
+
                                     <p class="card-text-price text-end">Price<?php echo $row["price"] ?>dkk</p>
                                 </div>
                                 <div class="card-footer text-end">
@@ -261,12 +152,13 @@ require_once("connection/conn.php");
     <style>
         .discountedItem {
             object-fit: contain;
+            margin-top: -30%;
         }
 
         #text-box {
             font-family: "Apple SD Gothic Neo";
             color: #434343;
-            margin-right: 50%;
+            margin-right: 40%;
             margin-left: 5%;
             line-height: 25px;
             font-size: 19px;
@@ -290,10 +182,10 @@ require_once("connection/conn.php");
 
 
         #cardtop {
-            width: 18%;
-            height: 15%;
-            margin-left: 78%;
+            width: 16%;
+            margin-left: 80%;
             margin-right: 5%;
+            margin-top: -8%;
             border-radius: 10% 10% 10% 10%;
             box-shadow: 0 8px 12px 0 rgba(0, 0, 0, 0.2), 0 10px 24px 0 rgba(0, 0, 0, 0.19);
 
@@ -323,12 +215,13 @@ require_once("connection/conn.php");
         .card-text {
             font-size: smaller;
             font-weight: light;
+
         }
 
 
         .embed-responsive .card-img-top {
             border-radius: 10% 10% 0 0;
-            height: 50%;
+            height: 10%;
 
 
 
@@ -337,10 +230,7 @@ require_once("connection/conn.php");
 
         .embed-responsive .card-img-top-top {
             border-radius: 10% 10% 0 0;
-            height: 10%;
-            width: 55%;
-
-
+            width: 100%;
 
         }
 
@@ -411,7 +301,7 @@ require_once("connection/conn.php");
 
         .discountedItem {
             margin-left: 62%;
-            width: 35%;
+            width: 34.5%;
         }
     </style>
 </body>
