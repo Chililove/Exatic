0055cce9de80f7570ff5b269174ce61676893cff
iNKidzy <nadq.nadia@gmail.com>
2022-11-12 23:32:58 +0100

    nav-profile

--- View/_partials/header.php
+++ View/_partials/header.php
@@ -48,9 +48,9 @@ $PageTitle = "Exatic";
                     <li class="nav-item">
                         <a class="nav-link" href="/contact">Contact</a>
                     </li>
-                    <li class="nav-item">
+                    <!-- <li class="nav-item">
                         <a class="nav-link" href="/profile">Profile</a>
-                    </li>
+                    </li> -->
 
                     <!-- Cart-preview -->
                     <li class="nav-item dropdown" style="position:absolute; right:6%;">
