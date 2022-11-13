2ea93f1ee0c263df0fc400fd790695e76174b465
Louise Chili <chili@Louises-MacBook-Pro.local>
2022-11-13 18:39:54 +0100

    unque constraint on email

--- sql/exaticDB.sql
+++ sql/exaticDB.sql
@@ -31,7 +31,7 @@ CREATE TABLE `User` (
     userID INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
     firstName VARCHAR(50) NOT NULL,
     lastName VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL unique,
     password VARCHAR(1000) NOT NUll,
     userType INTEGER,
     imagePath VARCHAR(1000),
