8e9958ec3c2e5ec8923527e95b600002dcf216d8
Suong <suong95@outlook.dk>
2022-11-17 21:28:26 +0100

    admin

--- View/productoverview.php
+++ View/productoverview.php
@@ -7,19 +7,14 @@ require $rootPath . "Controller/POverviewController.php";
 
 ?>
 
-<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
 
 <!doctype html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Product-Overview</title>
-</head>
 
-<body>
+
+
     <?php
 
     $overview = $_SERVER['QUERY_STRING'];
@@ -30,7 +25,7 @@ require $rootPath . "Controller/POverviewController.php";
 
     $overviewResult = mysqli_query($conn, $product_details);
     while ($row = mysqli_fetch_assoc($overviewResult)) { ?>
-        <div class="container">
+        <div class="container-fluid">
             <div class="row">
                 <div class="col-xs-4 item-photo">
                     <a href="/product-overview?<?php echo $row['productID']; ?>"><img class="card-img-top" src="/Exatic/assets/product/<?php echo $row['productImage'] ?>" alt="Card image top" /></a>
@@ -124,146 +119,5 @@ require $rootPath . "Controller/POverviewController.php";
             </div>
         </div>
     </div>
-</body>
 
 </html>
-
-<style lang="css">
-    ul>li {
-        margin-right: 25px;
-        font-weight: lighter;
-        cursor: pointer
-    }
-
-    li.active {
-        border-bottom: 3px solid silver;
-    }
-
-    .item-photo {
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        border-right: 1px solid #f6f6f6;
-    }
-
-    .menu-items {
-        list-style-type: none;
-        font-size: 11px;
-        display: inline-flex;
-        margin-bottom: 0;
-        margin-top: 20px
-    }
-
-    .btn-success {
-        width: 100%;
-        border-radius: 0;
-    }
-
-    .section {
-        width: 100%;
-        margin-left: -15px;
-        padding: 2px;
-        padding-left: 15px;
-        padding-right: 15px;
-        background: #f8f9f9
-    }
-
-    .title-price {
-        margin-top: 30px;
-        margin-bottom: 0;
-        color: black
-    }
-
-    .title-attr {
-        margin-top: 0;
-        margin-bottom: 0;
-        color: black;
-    }
-
-    .btn-minus {
-        cursor: pointer;
-        font-size: 7px;
-        display: flex;
-        align-items: center;
-        padding: 5px;
-        padding-left: 10px;
-        padding-right: 10px;
-        border: 1px solid gray;
-        border-radius: 2px;
-        border-right: 0;
-    }
-
-    .btn-plus {
-        cursor: pointer;
-        font-size: 7px;
-        display: flex;
-        align-items: center;
-        padding: 5px;
-        padding-left: 10px;
-        padding-right: 10px;
-        border: 1px solid gray;
-        border-radius: 2px;
-        border-left: 0;
-    }
-
-    div.section>div {
-        width: 100%;
-        display: inline-flex;
-    }
-
-    div.section>div>input {
-        margin: 0;
-        padding-left: 5px;
-        font-size: 10px;
-        padding-right: 5px;
-        max-width: 18%;
-        text-align: center;
-    }
-
-
-
-
-    .bsp_row-underline {
-
-        content: "";
-        display: block;
-        border-bottom: 2px solid #3cdb37;
-        margin-bottom: 20px
-    }
-
-    .bsp_deal-text {
-
-        margin-left: -10px;
-        font-size: 25px;
-        margin-bottom: 10px;
-        color: #000;
-        font-weight: 700;
-    }
-
-    .bsp_view-all {
-
-        margin-right: -10px;
-        font-size: 14px;
-        margin-top: 10px;
-
-    }
-
-    .bsp_padding-0 {
-
-        padding: 3px;
-    }
-
-    .bsp_bbb_item {
-
-        padding: 15px;
-        background-color: #fff;
-        box-shadow: 0px 1px 5px rgba(0, 0, 0, 0.1);
-        border: solid 1px #e8e8e8;
-
-    }
-
-    .bsp_card-text {
-
-        color: blue;
-    }
-</style>
