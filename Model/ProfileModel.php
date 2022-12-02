c474288f395f484e7e5e0b2669233bc85a521200
suon0001 <56873514+suon0001@users.noreply.github.com>
2022-12-02 12:50:28 +0100

    delete

--- Model/ProfileModel.php
+++ Model/ProfileModel.php
@@ -2,7 +2,5 @@
 class ProfileModel
 {
     public $user = "SELECT `userID`, `firstName`, `lastName`, `email`, `imagePath`  FROM `User` WHERE userID = 1";
-    public $product_details = "SELECT * FROM User u, Address a, postalCode p WHERE u.addressID = a.addressID AND a.postalCodeID = p.postalCodeID AND u.userType=0 LIMIT 1;";
-
 }
 $ProfileModel = new ProfileModel();
