cb7e89a170e55fd3a56da5f742d5509e030fd7a6
iNKidzy <nadq.nadia@gmail.com>
2022-12-02 14:16:47 +0100

    productAdminFix-pp

--- Model/AdminProductModel.php
+++ Model/AdminProductModel.php
@@ -3,6 +3,6 @@
 class AdminProductModel
 {
     public $product_list = "SELECT p.productID, p.title, p.price, p.stockQuantity, p.description, p.isNew, p.isDailySpecial, p.country, p.brand, p.productImage, p.timestamp, pt.productTypeID, pt.typeName, d.discountID, d.eventName, d.discountProcent
-                        FROM Product p, Producttype pt, Discount d WHERE p.productTypeID = pt.producttypeID AND p.discountID = d.discountID";
+                        FROM Product p, ProductType pt, Discount d WHERE p.productTypeID = pt.productTypeID AND p.discountID = d.discountID";
 }
 $AdminProductModel = new AdminProductModel();
