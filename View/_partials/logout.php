afa265f29cd470afaa944380c5182c797766e1ac
Louise Chili <chili@Louises-MacBook-Pro.local>
2022-12-15 00:20:44 +0100

    unset-session

--- View/_partials/logout.php
+++ View/_partials/logout.php
@@ -3,9 +3,10 @@ $_SESSION = array();
 
 // 3. Destroy the session cookie
 if (isset($_COOKIE[session_name()])) {
-    setcookie(session_name(), '', time() - 42000, '/');
+  setcookie(session_name(), '', time() - 42000, '/');
 }
-
+// 
+session_unset();
 // 4. Destroy the session
 session_destroy();
 
