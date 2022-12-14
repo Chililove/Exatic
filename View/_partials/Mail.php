6f0954e8cb80f7c891cac5fbd67c16cbf64e2452
iNKidzy <nadq.nadia@gmail.com>
2022-12-14 10:56:21 +0100

    header+mail-partly

--- View/_partials/Mail.php
+++ View/_partials/Mail.php
@@ -1,5 +1,5 @@
 <?php
-//IDK if this should be a Controller ? I think it could be moved to _partials
+
 if (isset($_POST["send_email"])) {
 
     $myemail = "exaticproject@gmail.com";
@@ -9,22 +9,24 @@ if (isset($_POST["send_email"])) {
     $lastName = $_POST['lastName'];
     $message = $_POST['message'] . "/n/n MVH /n" . $firstName . $lastName;
     $regexp = "/^[^0-9][A-z0-9_-]+([.][A-z0-9_]+)*[@][A-z0-9_]+([.][A-z0-9_-]+)*[.][A-z]{2,4}$/";
-    // $nameValid = "/^[A-Z][.]-$/";
-    //  } elseif (!preg_match($nameValid, $_POST['firstName'], $_POST['lastName'])) {
-    //   header("Location:http://localhost:8886/contact.php?msgid=4");
     $msgValid = "/^[A-z]{2,}$/";
     if (!isset($_POST['email'])) {
         header("Location:http://localhost:8886/contact.php?msgid=1");
-    } elseif (empty($_POST['email'] || empty($_POST['myemail']) || $_POST['subject']  || $_POST['firstName'] || $_POST['lastName'] || $_POST['message'])) {
-        header("Location:http://localhost/contact.php?msgid=2");
+    } elseif (empty(($_POST['myemail']) || $_POST['subject'] ||  $_POST['firstName'] || $_POST['lastName'] || $_POST['message'])) {
+        header("Location:http://localhost:8886/contact.php?msgid=2");
     } elseif (!preg_match($regexp, $_POST['email'])) {
         header("Location:http://localhost:8886/contact.php?msgid=3");
-        // } elseif (!preg_match($msgValid, $_POST['message'])) {
-        //     header("Location:http://localhost:8886/contact.php?msgid=5");
-        //
+    } elseif (!preg_match($msgValid, $_POST['message'])) {
+        header("Location:http://localhost:8886/contact.php?msgid=4");
     } else {
-        // mail($myemail, "From: $email\n", $subject, $firstName, $lastName, $message);
-        mail("From: $email\n", $subject, $firstName, $lastName, $message);
-        header("Location:SendMail.php");
+        //mail( string $myemail, string $subject , string $message [, string $additional_headers [, string $additional_parameters ]] )
+        mail($myemail, "From: $email\n", $subject, $firstName, $message);
+        // mail("From: $email\n", $subject, $firstName, $lastName, $message);
+        header("Location: SendMail.php"); // Only for localhost!!!!Doesn't work on one.com
+        //for one.com
+        // $url = "http://exatic.store/contact/../SendMail.php";
+        // echo ("<script>
+        //   location.href = '$url'
+        //</script>");
     }
 }
