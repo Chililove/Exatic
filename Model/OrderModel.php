fdc5d527cd9cd18adcea0f2241cc40b1682afafc
suon0001 <56873514+suon0001@users.noreply.github.com>
2022-12-15 20:39:49 +0100

    ADDDATE

--- Model/OrderModel.php
+++ Model/OrderModel.php
@@ -3,7 +3,7 @@
 class OrderModel
 {
 
-    public $orderInsert = "INSERT INTO `Order` (`dateOrdered`, `status`, `userID`) VALUES (NOW(), :status, :userID)";
+    public $orderInsert = "INSERT INTO `Order` (`dateOrdered`, `dateDelivered`, `status`, `userID`) VALUES (NOW(), ADDDATE(CURDATE(),5), :status, :userID)";
     public $orderDetailsInsert = "INSERT INTO `OrderDetail` (title,quantity, price, procent,orderID, productID, priceOne) VALUES (:title, :quantity, :price, :procent, :orderID, :productID, :priceOne)";
 }
 $OrderModel = new OrderModel();
