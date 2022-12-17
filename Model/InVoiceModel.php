a02b6db9dc4a603cbb2e11dfb21f9c9893de5249
suon0001 <56873514+suon0001@users.noreply.github.com>
2022-12-17 02:00:53 +0100

    invoice-correction

--- Model/InVoiceModel.php
+++ Model/InVoiceModel.php
@@ -3,13 +3,13 @@ $orderId = $_GET['orderID'];
 $ordersDetail = "SELECT * FROM `OrderDetail` WHERE orderID = $orderId";
 $ordersDate = "SELECT * FROM `Order` WHERE orderID = $orderId";
 $ordersTotal = "SELECT SUM(price) AS totalQuantity
-                FROM Orderdetail
+                FROM OrderDetail
                 WHERE orderID = $orderId";
-$ordersUser = "SELECT * FROM `Order` o, `User` u, `Address` a, `Postalcode` p WHERE o.userID = u.userID AND u.addressID = a.addressID AND a.postalCodeID = p.postalCodeID AND o.orderID = $orderId";
+$ordersUser = "SELECT * FROM `Order` o, `User` u, `Address` a, `PostalCode` p WHERE o.userID = u.userID AND u.addressID = a.addressID AND a.postalCodeID = p.postalCodeID AND o.orderID = $orderId";
 class InVoiceModel
 {
 
-public $orderCompany = "SELECT * FROM `CompanyInfo` c, `Address` a, `Postalcode` p WHERE c.addressID = a.addressID AND a.postalCodeID = p.postalCodeID";
+public $orderCompany = "SELECT * FROM `CompanyInfo` c, `Address` a, `PostalCode` p WHERE c.addressID = a.addressID AND a.postalCodeID = p.postalCodeID";
 }
 
 $InVoiceModel = new InVoiceModel();
